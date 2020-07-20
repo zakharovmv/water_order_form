18db686b6bb05e33e709f0e1a8f1d35b09b96215
Mikhail Zakharov <mzaharov@softbalance.ru>
2020-07-20 11:07:47 +0300

    exclude data.json from gitignore, change api_url

--- src/system/config.js
+++ src/system/config.js
@@ -1,6 +1,6 @@
 export default Object.freeze({
   lang: "ru-RU",
-  api_url: "http://gazprom.loc/api/",
+  api_url: "http://waterbases.loc/api/",
   yandex_maps_api_url: "https://geocode-maps.yandex.ru/1.x/",
   yandex_maps_api_key: "45ccaf60-7908-468d-adde-66b6dca72de5"
 });
